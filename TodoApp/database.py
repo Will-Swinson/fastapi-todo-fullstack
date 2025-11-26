78b4a6ba3900621624d9e4f5d7041f950923c4ac
Will Swinson <william.swinson01@gmail.com>
2025-11-25 20:36:45 -0600

    add env variable for db connection string.

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,7 +1,12 @@
+from dotenv import load_env
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://will@localhost:5432/todos_app"
+load_env()
+
+# Local testing "postgresql://will@localhost:5432/todos_app"
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
